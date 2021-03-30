4dd08abf6d5de1bd5031608a71fa958a4b4bf3db
maoyanmi <3319435445@qq.com>
2021-03-30 17:50:05 +0800

    模拟B修改了代码，但未提交就拉取远端代码，而远端代码和B修改了的代码出现冲突，此时要么先把B自己的代码提交再拉取远端代码，拉取的时候就可以解决冲突了；要么将自己的代码先stash后再拉取代码，最后再把自己的代码unstash出来，unstash的时候就可以解决冲突了。

--- src/com/mym/Main.java
+++ src/com/mym/Main.java
@@ -7,6 +7,10 @@ public class Main {
         System.out.println("努力努力！！");
         System.out.println("进步进步！！");
         System.out.println("修改了一个文件");
+        /*
+        * B修改后不提交又拉取了别人的代码，预期是修改后的代码不会改变，
+        * 因为代码拉取下拉后存放的地方是本地代码库，并不是本地磁盘
+        * */
         System.out.println("我是程序员A");
     }
 }
